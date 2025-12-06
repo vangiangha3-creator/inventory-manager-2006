9cec7ce663df346f20d7e643afc77eac2258f1c5
vangiangha3-creator <vangiangha3@gmail.com>
2025-12-06 12:53:17 +0700

    1

---
+++ inventory.py
@@ -0,0 +1,25 @@
+products = []
+def main():
+    while True:
+        print("\n--- QUẢN LÝ KHO HÀNG ---")
+        print("1. Nhập hàng mới")
+        print("2. Xem tồn kho")
+        print("3. Cảnh báo hết hàng")
+        print("4. Thoát")
+
+        choice = input("Chọn chức năng: ")
+
+        if choice == '1':
+            add_product()
+        elif choice == '2':
+            view_inventory()
+        elif choice == '3':
+            check_low_stock()
+        elif choice == '4':
+            print("Kết thúc chương trình.")
+            break
+        else:
+            print("Lựa chọn không hợp lệ.")
+
+if __name__ == "__main__":
+    main()
